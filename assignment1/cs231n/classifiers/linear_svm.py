2d8b41f5328b2398af0559a164fadded387a6519
Sergey Benditkis <sezhka@gmail.com>
2018-01-20 22:20:03 +0200

    Did some progress with svm loss and gradient

--- assignment1/cs231n/classifiers/linear_svm.py
+++ assignment1/cs231n/classifiers/linear_svm.py
@@ -35,11 +35,12 @@ def svm_loss_naive(W, X, y, reg):
       margin = scores[j] - correct_class_score + 1 # note delta = 1
       if margin > 0:
         loss += margin
+        dW[:,j] += X[i]
+        dW[:,y[i]] -= X[i]
 
   # Right now the loss is a sum over all training examples, but we want it
   # to be an average instead so we divide by num_train.
   loss /= num_train
-
   # Add regularization to the loss.
   loss += reg * np.sum(W * W)
 
@@ -51,7 +52,9 @@ def svm_loss_naive(W, X, y, reg):
   # loss is being computed. As a result you may need to modify some of the    #
   # code above to compute the gradient.                                       #
   #############################################################################
-
+  dW /= num_train
+  dW += reg*2*W
+  
 
   return loss, dW
 
@@ -70,7 +73,18 @@ def svm_loss_vectorized(W, X, y, reg):
   # Implement a vectorized version of the structured SVM loss, storing the    #
   # result in loss.                                                           #
   #############################################################################
-  pass
+  num_train = X.shape[0]
+  s = X.dot(W)
+  
+  s_correct = s[np.arange(num_train), y]
+  diff = np.maximum(0, (s.T-s_correct+1).T)
+
+  diff[diff<0] = 0
+  diff[np.arange(num_train), y] = 0
+
+  loss = np.sum(diff)
+  loss /= num_train
+
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
@@ -85,7 +99,11 @@ def svm_loss_vectorized(W, X, y, reg):
   # to reuse some of the intermediate values that you used to compute the     #
   # loss.                                                                     #
   #############################################################################
-  pass
+  khi = 1*(diff>0)
+  khi[np.arange(num_train), y] = -1*np.sum(khi, axis=1)
+  
+  dW+=X.T.dot(khi)
+  dW/=num_train
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
