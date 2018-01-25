9d7fabe717c329eff99f5a477eb35c149b6ae12e
Sergey Benditkis <sezhka@gmail.com>
2018-01-25 19:07:37 +0000

    finished everything. Still want to do some more trainings.

--- assignment1/cs231n/classifiers/neural_net.py
+++ assignment1/cs231n/classifiers/neural_net.py
@@ -209,7 +209,7 @@ class TwoLayerNet(object):
       #                             END OF YOUR CODE                          #
       #########################################################################
 
-      if verbose and it % 100 == 0:
+      if verbose and it % (num_iters / 10) == 0:
         print('iteration %d / %d: loss %f' % (it, num_iters, loss))
 
       # Every epoch, check train and val accuracy and decay learning rate.
